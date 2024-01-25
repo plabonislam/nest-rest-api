d8fea0ff7236795ac8989e7bfb97cbc584674d71
plabonislam <shahnurislamplabon@gmail.com>
2024-01-26 03:56:02 +0600

    type orm code is removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
-import config from 'ormconfig';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UserModule, CommentModule,TypeOrmModule.forRoot(config)],
+  imports: [UserModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class UserService {
 
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {
-   
-  }
+
   findOne(id: string) {
     return {
       user: {
